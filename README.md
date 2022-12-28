58fa4f5d9dbf463ee19d335e73dc5f4b4ad3682b
Jens Walter <jens@apimeister.com>
2022-12-28 17:56:46 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# dtz-theme-hugo
+# dtz-theme
 dtz theme for hugo
+
+## how to use
+
+```
+git submodule add https://github.com/DownToZero-Cloud/dtz-theme-hugo.git {website_dir}/themes/dtz-theme-hugo
+```
---
+++ static/js/dtz-context-dropdown.js
@@ -0,0 +1,26 @@
+class DtzContextDropdown extends HTMLElement {
+    constructor() {
+        super();
+    }
+    changeContext(evt) {
+        if (evt) evt.preventDefault();
+        let val = evt.target.value;
+        console.log('new value ' + val);
+        this.value = val;
+    }
+    async connectedCallback() {
+        let list = getContextListFromCookie();
+        let currentContext = getCurrentContext();
+        let str = "";
+        for (let contextId of list) {
+            let alias = getAliasForContext(contextId);
+            str += `<option value="${contextId}" ${currentContext == contextId ? 'selected' : ''}>${alias} (${contextId})</option>`;
+        }
+        this.innerHTML = `<select class="form-select" aria-label="choose context">
+        ${str}
+      </select>`;
+        this.value = currentContext;
+        this.querySelector('select').addEventListener('change', this.changeContext);
+    }
+}
+window.customElements.define('dtz-context-dropdown', DtzContextDropdown);
