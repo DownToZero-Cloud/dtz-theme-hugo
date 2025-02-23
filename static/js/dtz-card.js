68bc8fc6a460cf3c33280bf02b1fe4ef1cd235a4
Jens Walter <jens@apimeister.com>
2025-02-23 13:19:33 +0100

    Update dtz-card.js

--- static/js/dtz-card.js
+++ static/js/dtz-card.js
@@ -16,15 +16,22 @@ class DtzCard extends HTMLElement {
         styles.insertRule(`.dtz-heading {
             background-color: var(--dtz-light);
             text-align: center;
+            padding-top: 0.75em;
+            padding-bottom: 0.75em;
+            margin-block-start: 0;
+            margin-block-end: 0;
+            font-size: 1.25rem;
         }`);
         styles.insertRule(`.dtz-heading.loading {
-            background-color: var(--bs-secondary);
+            background-color: var(--bs-secondary-bg-subtle);
+            transition: background-color 1s, height 1s;
         }`);
         styles.insertRule(`.dtz-card {
             margin: 1em;
             width: 27em;
             border-radius: 2em;
             border: 2px solid var(--dtz-normal);
+            overflow: hidden;
         }`);
         return styles;
     }
@@ -34,9 +41,12 @@ class DtzCard extends HTMLElement {
         let title = this.getAttribute('title');
         let loading = this.getAttribute('loading');
         let title_str = title ? `<h5 class="dtz-heading ${loading ? 'loading' : ''}">${title}</h5>` : '';
+        let spinner = loading ? `<div class="spinner-border" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>`: "";
         shadow.innerHTML = `<div class="dtz-card">
             ${title_str}
-            <div style="margin: 1em;"><slot></slot></div>
+            <div style="margin: 1em;">${spinner}<slot></slot></div>
         </div>`;
     }
 }
