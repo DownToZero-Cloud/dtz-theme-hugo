79ae06a342c738f7f8b2409547ca07124e9be3ca
Jens Walter <jens@apimeister.com>
2025-02-23 12:27:44 +0100

    Update dtz-card.js

--- static/js/dtz-card.js
+++ static/js/dtz-card.js
@@ -13,18 +13,17 @@ class DtzCard extends HTMLElement {
     }
     styles() {
         const styles = new CSSStyleSheet();
-        styles.replaceSync(`.dtz-heading {
+        styles.insertRule(`.dtz-heading {
             background-color: var(--dtz-light);
             text-align: center;
         }`);
-        styles.replaceSync(`.dtz-heading.loading {
+        styles.insertRule(`.dtz-heading.loading {
             background-color: var(--bs-secondary);
         }`);
-        styles.replaceSync(`.dtz-card {
+        styles.insertRule(`.dtz-card {
             margin: 1em;
             width: 27em;
             border-radius: 2em;
-            padding: 1em;
             border: 2px solid var(--dtz-normal);
         }`);
         return styles;
@@ -36,7 +35,7 @@ class DtzCard extends HTMLElement {
         let loading = this.getAttribute('loading');
         shadow.innerHTML = `<div class="dtz-card">
             ${title ? `<h5 class="dtz-heading ${loading ? 'loading' : ''}">${title}</h5>` : ''}
-            <div style="margin: 1em;>
+            <div style="margin: 1em;">
             <slot></slot>
             </div>
         </div>`;
