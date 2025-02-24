4be6ce14f090bda0faf7c873223f338cb27d372c
Jens Walter <jens@apimeister.com>
2025-02-24 15:59:12 +0100

    add links

---
+++ content/links.md
@@ -0,0 +1,10 @@
+---
+title: "Home"
+components:
+- dtz-link
+---
+<div style="width: 200px;height: 300px;display: flex;flex-direction: column;justify-content: space-between;">
+    <dtz-link href="#">Click me</dtz-link>
+    <dtz-link class="secondary" href="#secondary">Click me</dtz-link>
+    <dtz-link class="danger" href="#danger">Click me</dtz-link>
+</div>
---
+++ static/js/dtz-link.js
@@ -0,0 +1,110 @@
+class DtzLink extends HTMLElement {
+    constructor() {
+        super();
+        this.shadow = this.attachShadow({ mode: "open" });
+    }
+    styles() {
+        const styles = new CSSStyleSheet();
+        styles.insertRule(`a {
+            width: 100%;
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: var(--bs-btn-padding-y) var(--bs-btn-padding-x);
+            font-family: var(--bs-btn-font-family);
+            font-size: var(--bs-btn-font-size);
+            font-weight: var(--bs-btn-font-weight);
+            line-height: var(--bs-btn-line-height);
+            color: var(--bs-btn-color);
+            text-align: center;
+            text-decoration: none;
+            vertical-align: middle;
+            user-select: none;
+            border: var(--bs-btn-border-width) solid var(--bs-btn-border-color);
+            border-radius: var(--bs-btn-border-radius);
+            background-color: var(--bs-btn-bg);
+            transition: color .15sease-in-out, background-color .15sease-in-out, border-color .15sease-in-out, box-shadow .15sease-in-out;
+        }`);
+        styles.insertRule(`a:hover {
+            color: var(--bs-btn-hover-color);
+            background-color: var(--bs-btn-hover-bg);
+            border-color: var(--bs-btn-hover-border-color);
+        }`);
+        styles.insertRule(`a.disabled, a:disabled {
+            color: var(--bs-btn-disabled-color);
+            pointer-events: none;
+            background-color: var(--bs-btn-disabled-bg);
+            border-color: var(--bs-btn-disabled-border-color);
+            opacity: var(--bs-btn-disabled-opacity);
+        }`);
+        styles.insertRule(`:host {
+            --bs-btn-color: var(--dtz-normal);
+            --bs-btn-border-color: var(--dtz-normal);
+            --bs-btn-hover-color: #fff;
+            --bs-btn-hover-bg: var(--dtz-normal);
+            --bs-btn-hover-border-color: var(--dtz-normal);
+            --bs-btn-focus-shadow-rgb: 13, 110, 253;
+            --bs-btn-active-color: #fff;
+            --bs-btn-active-bg: var(--dtz-normal);
+            --bs-btn-active-border-color: var(--dtz-normal);
+            --bs-btn-active-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+            --bs-btn-disabled-color: var(--dtz-normal);
+            --bs-btn-disabled-bg: transparent;
+            --bs-btn-disabled-border-color: var(--dtz-normal);
+            --bs-gradient: none;
+            --bs-btn-padding-x: 0.75rem;
+            --bs-btn-padding-y: 0.375rem;
+            --bs-btn-font-family: ;
+            --bs-btn-font-size: 1rem;
+            --bs-btn-font-weight: 400;
+            --bs-btn-line-height: 1.5;
+            --bs-btn-bg: transparent;
+            --bs-btn-border-width: 1px;
+            --bs-btn-border-radius: 0.375rem;
+            --bs-btn-box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.15), 0 1px 1px rgba(0, 0, 0, 0.075);
+            --bs-btn-disabled-opacity: 0.65;
+            --bs-btn-focus-box-shadow: 0 0 0 0.25rem rgba(var(--bs-btn-focus-shadow-rgb), .5);
+        }`);
+        styles.insertRule(`:host(.secondary) {
+            --bs-btn-color: #6c757d;
+            --bs-btn-border-color: #6c757d;
+            --bs-btn-hover-color: #fff;
+            --bs-btn-hover-bg: #6c757d;
+            --bs-btn-hover-border-color: #6c757d;
+            --bs-btn-focus-shadow-rgb: 108, 117, 125;
+            --bs-btn-active-color: #fff;
+            --bs-btn-active-bg: #6c757d;
+            --bs-btn-active-border-color: #6c757d;
+            --bs-btn-active-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+            --bs-btn-disabled-color: #6c757d;
+            --bs-btn-disabled-bg: transparent;
+            --bs-btn-disabled-border-color: #6c757d;
+            --bs-gradient: none;
+        }`);
+        styles.insertRule(`:host(.danger) {
+            --bs-btn-color: #dc3545;
+            --bs-btn-border-color: #dc3545;
+            --bs-btn-hover-color: #fff;
+            --bs-btn-hover-bg: #dc3545;
+            --bs-btn-hover-border-color: #dc3545;
+            --bs-btn-focus-shadow-rgb: 220, 53, 69;
+            --bs-btn-active-color: #fff;
+            --bs-btn-active-bg: #dc3545;
+            --bs-btn-active-border-color: #dc3545;
+            --bs-btn-active-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+            --bs-btn-disabled-color: #dc3545;
+            --bs-btn-disabled-bg: transparent;
+            --bs-btn-disabled-border-color: #dc3545;
+            --bs-gradient: none;
+        }`);
+        return styles;
+    }
+    connectedCallback() {
+        let shadow = this.shadow;
+        shadow.adoptedStyleSheets = [this.styles()];
+        let href = this.getAttribute("href");
+        shadow.innerHTML = `<a href="${href}"><slot></slot></a>`;
+    }
+}
+window.customElements.define('dtz-link', DtzLink);
