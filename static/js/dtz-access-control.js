1c2d28e21041b2300685186131868adebb1a5099
Jens Walter <jens@apimeister.com>
2025-08-18 22:41:41 +0200

    allow for two-state switcher

--- static/js/dtz-access-control.js
+++ static/js/dtz-access-control.js
@@ -1,5 +1,5 @@
 export class DtzAccessControl extends HTMLElement {
-    #state = 'no-access';
+    #state = 'red-state';
 
     constructor() {
         super();
@@ -10,15 +10,15 @@ export class DtzAccessControl extends HTMLElement {
         styles.insertRule(`:host {
             display: inline-block;
         }`);
-        styles.insertRule(`:host([state="no-access"]) {
+        styles.insertRule(`:host([state="red-state"]) {
             --background-color: hsl(354, 70%, 83%);
             --border-color: hsl(354, 70%, 63%);
         }`);
-        styles.insertRule(`:host([state="read-only"]) {
+        styles.insertRule(`:host([state="yellow-state"]) {
             --background-color: hsl(45, 100%, 85%);
             --border-color: hsl(45, 100%, 55%);
         }`);
-        styles.insertRule(`:host([state="write"]) {
+        styles.insertRule(`:host([state="green-state"]) {
             --background-color: hsl(105, 100%, 90%);
             --border-color: hsl(105, 100%, 30%);
         }`);
@@ -34,15 +34,21 @@ export class DtzAccessControl extends HTMLElement {
             padding-bottom: 0.5rem;
             background-color: var(--background-color);
             transition: border-color 4s, background-color 4s;
+            display: grid;
+            grid-template-columns: auto 1fr 1fr 1fr;
+        }`);
+        styles.insertRule(`.root.two-state {
+            grid-template-columns: auto 1fr 1fr;
         }`);
         styles.insertRule(`.option {
             cursor: pointer;
             text-transform: uppercase;
-            width: 6.5rem;
             border-right: 2px solid var(--border-color);
             display: inline-block;
             text-align: center;
             transition: border-color 4s;
+            padding-left: 0.5rem;
+            padding-right: 0.5rem;
         }`);
         styles.insertRule(`.option.active {
             font-weight: bold;
@@ -50,12 +56,14 @@ export class DtzAccessControl extends HTMLElement {
         styles.insertRule(`.option:last-child {
             border-right: 0;
         }`);
+        styles.insertRule(`.root.two-state .option:nth-child(3) {
+            display: none;
+        }`);
         return styles;
     }
-    changeState(event) {
-        let state = event.target.dataset.state;
+    changeState(state) {
         this.#state = state;
-        this.shadow.querySelector(".option.active").classList.remove("active");
+        this.shadow.querySelector(".option.active")?.classList.remove("active");
         this.shadow.querySelector(`.option[data-state="${state}"]`).classList.add("active");
         this.setAttribute('state', state);
         this.dispatchEvent(new CustomEvent('valueChanged', { detail: state }));
@@ -66,19 +74,30 @@ export class DtzAccessControl extends HTMLElement {
         shadow.adoptedStyleSheets = [this.styles()];
         shadow.innerHTML = `<div class="root">
           <span class="label"><slot></slot></span>
-          <span class="option ${this.#state == 'no-access' ? 'active' : ''}" data-state="no-access">
-            No Access
-          </span>
-          <span class="option ${this.#state == 'read-only' ? 'active' : ''}" data-state="read-only">
-            Read Only
-          </span>
-          <span class="option ${this.#state == 'write' ? 'active' : ''}" data-state="write">
-            Write
-          </span>
+          <slot name="red" class="option ${this.#state == 'red-state' ? 'active' : ''}" data-state="red-state"></slot>
+          <slot name="yellow" class="option ${this.#state == 'yellow-state' ? 'active' : ''}" data-state="yellow-state"></slot>
+          <slot name="green" class="option ${this.#state == 'green-state' ? 'active' : ''}" data-state="green-state"></slot>
         </div>`;
-        this.shadow.querySelectorAll(".option").forEach(option => {
-            option.addEventListener('click', this.changeState.bind(this));
+        this.shadow.querySelector(`.option[data-state="red-state"]`).addEventListener('click', (event) =>{
+            event.preventDefault();
+            this.changeState("red-state");
         });
+        this.shadow.querySelector(`.option[data-state="yellow-state"]`).addEventListener('click', (event) =>{
+            event.preventDefault();
+            this.changeState("yellow-state");
+        });
+        this.shadow.querySelector(`.option[data-state="green-state"]`).addEventListener('click', (event) =>{
+            event.preventDefault();
+            this.changeState("green-state");
+        });
+
+        const hasInitialContent = this.shadow.querySelector(`.option[data-state="yellow-state"]`).assignedNodes().some(node =>
+            node.nodeType === Node.ELEMENT_NODE ||
+            (node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '')
+        );
+        if(!hasInitialContent) {
+            this.shadow.querySelector(".root").classList.add("two-state");
+        }
     }
 }
 window.customElements.define('dtz-access-control', DtzAccessControl);
--- storybook/src/DtzAccessControl.stories.js
+++ storybook/src/DtzAccessControl.stories.js
@@ -2,11 +2,33 @@ import { DtzAccessControl } from '../../static/js/dtz-access-control.js';
 
 export default {
     component: 'dtz-access-control',
-    render: (args) => `<dtz-access-control>${args.slot}</dtz-access-control>`,
+    render: (args) => `<dtz-access-control>
+       ${args.slot}
+       ${args.slots ? `${args.slots}` : ''}
+    </dtz-access-control>`,
 };
 
 export const Default = {
     args: {
         slot: 'Members',
+        slots: `<div slot="red">No Access</div>
+                <div slot="green">Chat</div>`,
+    },
+};
+
+export const Null = {
+    args: {
+        slot: '&lt;NULL&gt;',
+        slots: `<div slot="red">No Access</div>
+                <div slot="yellow">Chat</div>
+                <div slot="green">Resources</div>`,
+    },
+};
+
+export const Support = {
+    args: {
+        slot: 'Support',
+        slots: `<div slot="red">No Access</div>
+                <div slot="green">Chat</div>`,
     },
 };
